1b506a69e4e99a1ed16eedf6d4d2081d1b90bb75
lucong <332553882@qq.com>
2019-11-11 17:49:12 +0800

    update

--- app/controller/info.js
+++ app/controller/info.js
@@ -8,20 +8,23 @@ class InfoController extends Controller {
    * @param {*} ret 
    */
   async detailGet(args, ret) {
-    this._authByToken(args, ret)
+    let authRet = await this._authByToken(args, ret)
+    if (authRet.code !== 0) {
+      return authRet
+    }
+
     this.LOG.info(args.uuid, 'detailGet', args)
 
     let userModel = new this.MODELS.userModel
-    let userId = args.user_id
+    let userId = args.UID
     this.LOG.info(args.uuid, 'detailGet|userId', userId)
     let user = await userModel.model().findByPk(userId, {
-      attributes: ['username', 'nickname', 'realname', 'email', 'mobile', 'sex', 'birth', 'avatar']
+      attributes: ['username', 'nickname', 'realname', 'email', 'mobile', 'sex', 'birth', 'avatar', 'uuid']
     })
     this.LOG.info(args.uuid, 'detailGet|user', user)
     ret.data = user
     return ret
   }
-
 
 }
 
