936a14630f907fdf2214b3ad89340b75279cedb7
lucong <332553882@qq.com>
2019-11-24 19:13:55 +0800

    update

--- app/controller/invite.js
+++ app/controller/invite.js
@@ -35,6 +35,42 @@ class InviteController extends Controller {
     }
     return ret
   }
+
+  async list(args, ret) {
+    this.LOG.info(args.uuid, '/list', args)
+    let authRet = await this._authByToken(args, ret)
+    if (authRet.code != 0) {
+      return authRet
+    }
+
+    let userId = args.UID
+
+    let page = args.page || 1
+    let limit = args.limit || 0
+    let userModel = new this.MODELS.userModel
+
+    let where = {}
+    let opts = {}
+    where.pid = userId
+
+    opts.where = where
+    opts.attributes = ['id', 'username', 'mobile', 'create_time', 'uuid']
+
+    if (limit) {
+      opts.offset = (page - 1) * limit
+      opts.limit = limit
+    }
+
+    opts.order = [
+      ['create_time', 'desc']
+    ]
+
+    let data = await userModel.model().findAndCountAll(opts)
+    this.LOG.info(args.uuid, '/list data', data)
+    ret.data = data
+    return ret
+
+  }
 }
 
 module.exports = InviteController
